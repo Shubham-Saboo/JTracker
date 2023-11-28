9c4dd99e3e20e3752dbcd90aed9fcf61f518c0e5
RajatChandak2000 <rajatchandak2000@gmail.com>
2023-11-27 21:16:28 -0500

    testing tests 9

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,6 +1,6 @@
 import unittest
 from app import create_app
-
+from unittest.mock import patch, MagicMock
 class TestApp(unittest.TestCase):
     def setUp(self):
         self.app = create_app().test_client()
@@ -24,7 +24,7 @@ class TestApp(unittest.TestCase):
         }
         response = self.app.post('/users/signup', json=payload)
         self.assertEqual(response.status_code, 200)
-        # Additional assertions based on the expected behavior of your app
+   
 
     def test_login(self):
         # Test user login
@@ -34,9 +34,47 @@ class TestApp(unittest.TestCase):
         }
         response = self.app.post('/users/login', json=payload)
         self.assertEqual(response.status_code, 200)
-        # Additional assertions based on the expected behavior of your app
+    
 
-    # Add more test cases for other endpoints as needed
+
+    @patch('app.Users.objects')
+    def test_get_profile(self, mock_user_objects):
+        # Mocking the Users.objects method to return a dummy user object
+        dummy_user = {
+            "applications": [
+                {"status": '1'},
+                {"status": '2'},
+                {"status": '3'},
+                {"status": '4'}
+            ],
+            "username": "test_user",
+            "fullName": "Test User",
+            "email": "test@example.com",
+            "skills": ["Python", "Flask"],
+            "workExperience": ["Test Work Experience"],
+            "education": ["Test Education"]
+        }
+        mock_user_objects.return_value = MagicMock(first=lambda: dummy_user)
+
+        response = self.app.get('/users/profile', headers={"Authorization": "Bearer mock_token"})
+        self.assertEqual(response.status_code, 200)
+
+        expected_stats = {
+            'wishlist': 1,
+            'applied': 1,
+            'waiting_for_referral': 1,
+            'rejected': 1,
+            'username': 'test_user',
+            'fullname': 'Test User',
+            'email': 'test@example.com',
+            'skills': ['Python', 'Flask'],
+            'workExp': 'Test Work Experience',
+            'edu': 'Test Education'
+        }
+        self.assertEqual(response.json, expected_stats)
+
+    
+      
 
 if __name__ == '__main__':
     unittest.main()
